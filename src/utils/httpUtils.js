5c210adc0896b41d56133bbfce284978d4e90f5d
ABang <413318038@qq.com>
2021-10-28 18:04:49 +0800

    take sensilet version in http request

--- src/utils/httpUtils.js
+++ src/utils/httpUtils.js
@@ -3,6 +3,10 @@ let httpUtils = {};
 const axios = require('axios');
 
 httpUtils.get = function (url,params){
+    if(!params)
+        params = {};
+    //接口请求带上版本号
+    params.versionCode = config.versionCode;
     return new Promise((resolve, reject) => {
         axios.get(url,{params}).then(res=>{
             if(res.status===200){
